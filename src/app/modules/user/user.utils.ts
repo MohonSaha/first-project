bbbe3e40fdc63ea397945e45ac245ef177a80ead
MohonSaha <practicewithmohon@gmail.com>
2023-12-06 17:39:42 +0600

    fix generated id related bug

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -17,14 +17,30 @@ const findLastStudentId = async () => {
     })
     .lean()
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+  return lastStudent?.id ? lastStudent.id : undefined
 }
 
 // year, semestercode, 4 digit number
 export const generateStudentId = async (payLoad: TAcademicSemester) => {
   // 4 Digit number ==================
   // for the first time
-  const currentId = (await findLastStudentId()) || (0).toString()
+  let currentId = (0).toString() // 0000
+
+  const lastStudentId = await findLastStudentId()
+  //2030 01 0001
+  const lastStudnetSemesterCode = lastStudentId?.substring(4, 6) // 01
+  const lastStudnetYear = lastStudentId?.substring(0, 4) // 2030
+  const currentSemesterCode = payLoad.code
+  const currentYear = payLoad.year
+
+  if (
+    lastStudentId &&
+    lastStudnetSemesterCode === currentSemesterCode &&
+    lastStudnetYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6) // 0001
+  }
+
   //   increment
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0')
 
