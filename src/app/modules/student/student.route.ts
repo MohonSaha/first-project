393595e2cb9a58ad7c6c85433affcc7e1d95001b
MohonSaha <practicewithmohon@gmail.com>
2024-02-17 07:17:45 +0600

    refector faculty module

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -7,20 +7,29 @@ import { USER_ROLE } from '../user/user.constant'
 
 const router = express.Router()
 
-router.get('/', StudentControllers.getAllStudents)
+router.get(
+  '/',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  StudentControllers.getAllStudents,
+)
 
 router.get(
   '/:id',
-  auth(USER_ROLE.student, USER_ROLE.faculty, USER_ROLE.admin),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
   StudentControllers.getSingleStudent,
 )
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(studentValidations.updateStudentValidationSchema),
   StudentControllers.updateStudent,
 )
 
-router.delete('/:id', StudentControllers.deleteStudent)
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  StudentControllers.deleteStudent,
+)
 
 export const StudentRoutes = router
